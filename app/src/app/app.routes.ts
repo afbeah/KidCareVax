58bac8dd6a8b8b09836e4e3ed0a4a482fd6ad6a2
Beatriz França <beahabreu@gmail.com>
2026-06-20 17:10:05 -0300

    feat: configura arquitetura inicial e navegação

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'landing',
     pathMatch: 'full',
   },
-];
+  {
+    path: 'landing',
+    loadComponent: () =>
+      import('./pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () => 
+      import('./pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () => 
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+]
---
+++ app/src/app/pages/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss'],
+  standalone: true,
+  imports: [RouterModule],
+})
+
+export class LadingComponent {}
