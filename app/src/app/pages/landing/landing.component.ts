7206fd8281271e638892131b8e2201e9f0fb2c31
Beatriz França <beahabreu@gmail.com>
2026-06-26 20:32:37 -0300

    refactor: moderniza a landing

--- app/src/app/pages/landing/landing.component.ts
+++ app/src/app/pages/landing/landing.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, IonContent],
 })
 
 export class LandingComponent {}
